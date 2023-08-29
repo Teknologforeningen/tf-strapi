395029f3e04bf855fb60a314ffd9076fda1b0924
Jonas Bäck <jonas.back@twoday.com>
2023-08-29 15:40:36 +0300

    Fix private page populate query

--- src/api/private-page/controllers/private-page.ts
+++ src/api/private-page/controllers/private-page.ts
@@ -15,10 +15,12 @@ export default factories.createCoreController(
     },
     async findOne(ctx) {
       const { id: slug } = ctx.params;
+      const { populate } = ctx.query as any; // :D (Strapi typing is wrong, don't want to fix properly // Bäck)
       const entity = await strapi.db
         .query("api::private-page.private-page")
         .findOne({
           where: { slug },
+          populate,
         });
       return this.transformResponse(entity);
     },
