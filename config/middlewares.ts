7bcd5fddad454ee238803309169ca89f301985f5
Jonas Bäck <jonas.c.back@gmail.com>
2023-09-02 15:41:22 +0300

    Add test domain as allowed CORS origin

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,27 +1,36 @@
 export default [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
+  "strapi::errors",
+  "strapi::security",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
   {
-    name: 'strapi::cors',
+    name: "strapi::cors",
     config: {
       enabled: true,
-      headers: '*',
-      origin: ['http://localhost:1337', 'http://localhost:3000', 'https://cms.tf.fi', 'https://tf.fi', 'https://id.tf.fi']
-    }
+      headers: "*",
+      origin: [
+        "http://localhost:1337",
+        "http://localhost:3000",
+        "https://cms.tf.fi",
+        "https://tf.fi",
+        "https://id.tf.fi",
+        "https://test.tf.fi",
+      ],
+    },
   },
   {
-    name: 'strapi::session',
+    name: "strapi::session",
     config: {
       store: {
         async get(key: string) {
-          const sessionInfo = await strapi.db.query("api::session.session").findOne({
-            select: ['session', 'expires'],
-            where: { key },
-          });
+          const sessionInfo = await strapi.db
+            .query("api::session.session")
+            .findOne({
+              select: ["session", "expires"],
+              where: { key },
+            });
 
           if (!sessionInfo) {
             return;
@@ -34,10 +43,12 @@ export default [
           return sessionInfo.session;
         },
         async set(key: string, session: unknown, maxAge: number) {
-          const sessionInfo = await strapi.db.query("api::session.session").findOne({
-            select: ['session', 'expires'],
-            where: { key },
-          });
+          const sessionInfo = await strapi.db
+            .query("api::session.session")
+            .findOne({
+              select: ["session", "expires"],
+              where: { key },
+            });
 
           if (sessionInfo) {
             await strapi.db.query("api::session.session").update({
@@ -48,23 +59,23 @@ export default [
               where: { key },
             });
           } else {
-            await strapi.service('api::session.session').create({
+            await strapi.service("api::session.session").create({
               data: {
                 key,
                 session,
                 expires: new Date(new Date().getTime() + maxAge * 1000),
-              }
-            })
+              },
+            });
           }
         },
         async destroy(key: string) {
           await strapi.db.query("api::session.session").delete({
-            where: { key }
+            where: { key },
           });
         },
       },
     },
   },
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::favicon",
+  "strapi::public",
 ];
